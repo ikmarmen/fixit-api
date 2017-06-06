ce4a1db3fc3e3f0d6c632796e1dfb7e8288ab961
armen.sargsyan <armen.sargsyan@margasoft.com>
2017-06-06 16:02:57 +0400

    Added comments and bids in post schema

--- models/post.js
+++ models/post.js
@@ -3,57 +3,115 @@ const validator = require('validator');
 const mongoose = require('mongoose');
 
 const photoScheme = mongoose.Schema({
-  data: Buffer,
-  width: Number,
-  height: Number,
+    data: Buffer,
+    width: Number,
+    height: Number,
 });
 
 const addressScheme = mongoose.Schema({
-  street:  String,
-  city: String,
-  state: String,
-  country: String,
-  zip: Number,
+    street: String,
+    city: String,
+    state: String,
+    country: String,
+    zip: Number,
+});
+
+const bidScheme = mongoose.Schema({
+    price: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    }
+});
+
+const commentSchema = mongoose.Schema({
+    body: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    replies: [
+        { type: Schema.ObjectId, ref: 'Person' }
+    ],
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    }
 });
 
 const postScheme = mongoose.Schema({
-  title: {
-    type: String,
-    index: true,
-    minlength: 5,
-    maxlength: 50,
-    required: [true, 'Post title is required']
-  },
+    title: {
+        type: String,
+        index: true,
+        minlength: 5,
+        maxlength: 50,
+        required: [true, 'Post title is required']
+    },
 
-  description: {
-    type: String,
-    index: true,
-    maxlength: 500,
-  },
+    description: {
+        type: String,
+        index: true,
+        maxlength: 500,
+    },
 
-  //geospatial index (https://docs.mongodb.com/manual/applications/geospatial-indexes/)
-  loc: {
-    type: [Number], // [longitude, latitude]
-    index: '2d',
-  },
+    //geospatial index (https://docs.mongodb.com/manual/applications/geospatial-indexes/)
+    loc: {
+        type: [Number], // [longitude, latitude]
+        index: '2d',
+    },
 
-  address: {
-    type: addressScheme,
-    required: true,
-  },
+    address: {
+        type: addressScheme,
+        required: true,
+    },
 
-  photos: [photoScheme],
+    photos: [photoScheme],
 
-  createdAt: {
-    type: Date,
-    required: true,
-    default: new Date(),
-  },
+    comments: [commentSchema],
 
-  updatedAt: {
-    type: Date,
-    required: true,
-    default: new Date(),
-  }
+    bids: [bidScheme],
 
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    },
+
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+    }
 });
